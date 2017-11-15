ce2b8569a82c0a24b2a5e7fda9b34f65a6fe7f59
Maryam <aminian.mary@gmail.com>
2017-11-15 10:09:16 -0500

    Neural_SRL + saves best epoch + small bug fix

---
+++ src/parser.py
@@ -0,0 +1,82 @@
+import os
+import pickle
+import time
+import utils
+from optparse import OptionParser
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("--train", dest="conll_train", help="Annotated CONLL train file", metavar="FILE", default=None)
+    parser.add_option("--dev", dest="conll_dev", help="Annotated CONLL dev file", metavar="FILE", default='')
+    parser.add_option("--input", dest="input", help="Annotated CONLL test file", metavar="FILE", default=None)
+    parser.add_option("--output", dest="output", help="output file", metavar="FILE", default=None)
+    parser.add_option("--params", dest="params", help="Parameters file", metavar="FILE", default="params")
+    parser.add_option("--extrn", dest="external_embedding", help="External embeddings", metavar="FILE")
+    parser.add_option("--model", dest="model", help="Load/Save model file", metavar="FILE", default="model")
+    parser.add_option("--d_w", type="int", dest="d_w", default=100)
+    parser.add_option("--d_l", type="int", dest="d_l", default=100)
+    parser.add_option("--d_pos", type="int", dest="d_pos", default=16)
+    parser.add_option("--d_h", type="int", dest="d_h", default=512)
+    parser.add_option("--d_r", type="int", dest="d_r", default=128)
+    parser.add_option("--d_prime_l", type="int", dest="d_prime_l", default=128)
+    parser.add_option("--k", type="int", dest="k", default=4)
+    parser.add_option("--batch", type="int", dest="batch", default=10)
+    parser.add_option("--alpha", type="float", dest="alpha", default=0.25)
+    parser.add_option("--beta2", type="float", dest="beta2", default=0.999)
+    parser.add_option("--beta1", type="float", dest="beta1", default=0.9)
+    parser.add_option("--eps", type="float", dest="eps", default=0.00000001)
+    parser.add_option("--learning_rate", type="float", dest="learning_rate", default=0.001)
+    parser.add_option("--epochs", type="int", dest="epochs", default=30)
+    parser.add_option("--outdir", type="string", dest="outdir", default="results")
+    parser.add_option("--dynet-autobatch", type="int", default=1)
+    parser.add_option("--dynet-mem", type="int", default=10240)
+    parser.add_option("--save_epoch", action="store_true", dest="save_epoch", default=False, help='Save each epoch.')
+    parser.add_option("--region", action="store_false", dest="region", default=True, help='Use predicate boolean flag.')
+
+    (options, args) = parser.parse_args()
+    print 'Using external embedding:', options.external_embedding
+    from srl import SRLLSTM
+
+    if options.conll_train:
+        print 'Preparing vocab'
+        print options
+        words, lemma_count,w2i, pos, semRels, pl2i = utils.vocab(options.conll_train)
+        with open(os.path.join(options.outdir, options.params), 'w') as paramsfp:
+            pickle.dump((words,lemma_count,w2i, pos, semRels, pl2i, options), paramsfp)
+        print 'Finished collecting vocab'
+
+        print 'Initializing blstm srl:'
+        best_f_score = 0.0
+        parser = SRLLSTM(words,lemma_count, pos, semRels, w2i, pl2i, options)
+        for epoch in xrange(options.epochs):
+            print 'Starting epoch', epoch
+            parser.Train(options.conll_train)
+
+            if options.conll_dev != '':
+                start = time.time()
+                utils.write_conll(os.path.join(options.outdir, options.model) + str(epoch+1)+ '.txt', parser.Predict(options.conll_dev))
+                os.system(
+                    'perl src/utils/eval.pl -g ' + options.conll_dev + ' -s ' +  os.path.join(options.outdir, options.model) + str(epoch+1)+ '.txt' + ' > ' +  os.path.join(options.outdir, options.model) + str(epoch+1)+ '.eval &')
+                print 'Finished predicting dev; time:', time.time() - start
+
+            labeled_f, unlabeled_f = utils.get_scores(
+                os.path.join(options.outdir, options.model) + str(epoch + 1) + '.eval')
+            print 'epoch: ' + str(epoch) + '-- labeled F1: ' + str(labeled_f) + ' Unlabaled F: ' + str(unlabeled_f)
+            if float(labeled_f) > best_f_score:
+                parser.Save(os.path.join(options.outdir, options.model))
+                best_f_score = float(labeled_f)
+                best_epoch = epoch
+
+        print 'Best epoch: ' + str(best_epoch)
+
+    if options.input and options.output:
+        with open(os.path.join(options.outdir, options.params), 'r') as paramsfp:
+            words,lemma_count,w2i, pos, semRels, pl2i, stored_opt = pickle.load(paramsfp)
+        stored_opt.external_embedding = options.external_embedding
+        parser = SRLLSTM(words,lemma_count,pos, semRels, w2i, pl2i, stored_opt)
+        parser.Load(os.path.join(options.outdir, options.model))
+        ts = time.time()
+        pred = list(parser.Predict(options.input))
+        te = time.time()
+        utils.write_conll(options.output, pred)
+        print 'Finished predicting test', te - ts
---
+++ src/utils.py
@@ -0,0 +1,110 @@
+from collections import Counter, defaultdict
+import re, codecs
+
+class ConllStruct:
+    def __init__(self, entries, predicates):
+        self.entries = entries
+        self.predicates = predicates
+
+    def __len__(self):
+        return len(self.entries)
+
+class ConllEntry:
+    def __init__(self, id, form, lemma, pos, sense='_', parent_id=-1, relation='_', predicateList=dict(),
+                 is_pred=False):
+        self.id = id
+        self.form = form
+        self.lemma = lemma
+        self.norm = normalize(form)
+        self.lemmaNorm = normalize(lemma)
+        self.pos = pos.upper()
+        self.parent_id = parent_id
+        self.relation = relation
+        self.predicateList = predicateList
+        self.sense = sense
+        self.is_pred = is_pred
+
+    def __str__(self):
+        entry_list = [str(self.id+1), self.form, self.lemma, self.lemma, self.pos, self.pos, '_', '_',
+                      str(self.parent_id),
+                      str(self.parent_id), self.relation, self.relation,
+                      '_' if self.sense == '_' else 'Y',
+                      self.sense]
+        for p in self.predicateList.values():
+            entry_list.append(p)
+        return '\t'.join(entry_list)
+
+def vocab(conll_path):
+    wordsCount = Counter()
+    posCount = Counter()
+    semRelCount = Counter()
+    lemma_count = Counter()
+
+    for sentence in read_conll(conll_path):
+        wordsCount.update([node.norm for node in sentence.entries])
+        posCount.update([node.pos for node in sentence.entries])
+        for node in sentence.entries:
+            if node.predicateList == None:
+                continue
+            if node.is_pred:
+                lemma_count.update([node.lemma])
+            for pred in node.predicateList.values():
+                if pred!='?':
+                    semRelCount.update([pred])
+    return (wordsCount, lemma_count, {w: i for i, w in enumerate(wordsCount)},
+            {p: i for i, p in enumerate(posCount)}, semRelCount.keys(),
+            {l: i for i, l in enumerate(lemma_count)})
+
+def read_conll(fh):
+    sentences = codecs.open(fh, 'r').read().strip().split('\n\n')
+    read = 0
+    for sentence in sentences:
+        words = []
+        predicates = list()
+        entries = sentence.strip().split('\n')
+        for entry in entries:
+            spl = entry.split('\t')
+            predicateList = dict()
+            is_pred = False
+            if spl[12] == 'Y':
+                is_pred = True
+                predicates.append(int(spl[0]) - 1)
+
+            for i in range(14, len(spl)):
+                predicateList[i - 14] = spl[i]
+
+            words.append(
+                ConllEntry(int(spl[0]) - 1, spl[1], spl[3], spl[5], spl[13], int(spl[9]), spl[11], predicateList,
+                           is_pred))
+        read += 1
+        yield ConllStruct(words, predicates)
+    print read, 'sentences read.'
+
+def write_conll(fn, conll_structs):
+    with codecs.open(fn, 'w') as fh:
+        for conll_struct in conll_structs:
+            for i in xrange(len(conll_struct.entries)):
+                entry = conll_struct.entries[i]
+                fh.write(str(entry))
+                fh.write('\n')
+            fh.write('\n')
+
+numberRegex = re.compile("[0-9]+|[0-9]+\\.[0-9]+|[0-9]+[0-9,]+");
+def normalize(word):
+    return 'NUM' if numberRegex.match(word) else word.lower()
+
+def get_scores(fp):
+    line_counter =0
+    labeled_f = 0
+    unlabeled_f = 0
+    with codecs.open(fp, 'r') as fr:
+        for line in fr:
+            line_counter+=1
+            if line_counter == 10:
+                spl = line.strip().split(' ')
+                labeled_f = spl[len(spl)-1]
+            if line_counter==13:
+                spl = line.strip().split(' ')
+                unlabeled_f = spl[len(spl) - 1]
+    return (labeled_f, unlabeled_f)
+
